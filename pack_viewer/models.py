825bc8dc408e4ddebe146182c801f6e2e4c9fb44
jottunn <luci_u25@yahoo.com>
2014-05-22 12:48:16 +0300

    mizerie

--- pack_viewer/models.py
+++ pack_viewer/models.py
@@ -5,17 +5,18 @@ from django.db import models
 
 
 class Cards(models.Model):
-    set = models.ForeignKey('Sets', blank=True, null=True)
-    set_number = models.IntegerField(blank=True, null=True)
-    name = models.CharField(max_length=60, blank=True)
-    type = models.CharField(max_length=60, blank=True)
-    rarity = models.CharField(max_length=60, blank=True)
-    cost = models.CharField(max_length=60, blank=True, null=True)
-    cmc = models.IntegerField(blank=True, null=True)
-    power = models.CharField(max_length=60, blank=True, null=True)
-    text = models.CharField(max_length=1000, blank=True, null=True)
+    set         = models.ForeignKey('Sets', blank=True, null=True)
+    set_number  = models.IntegerField(blank=True, null=True)
+    name        = models.CharField(max_length=60, blank=True)
+    type        = models.CharField(max_length=60, blank=True)
+    rarity      = models.CharField(max_length=60, blank=True)
+    cost        = models.CharField(max_length=60, blank=True, null=True)
+    cmc         = models.IntegerField(blank=True, null=True)
+    power       = models.CharField(max_length=60, blank=True, null=True)
+    toughness   = models.CharField(max_length=60, blank=True, null=True)
+    text        = models.CharField(max_length=1000, blank=True, null=True)
     gatherer_id = models.IntegerField(blank=True, null=True)
-    img = models.CharField(max_length=30, blank=True)
+    img         = models.CharField(max_length=30, blank=True)
     
     class Meta:
         verbose_name_plural = "Cards"
@@ -25,9 +26,9 @@ class Cards(models.Model):
 
 
 class Sets(models.Model):
-    name = models.CharField(max_length=60, blank=True)
+    name           = models.CharField(max_length=60, blank=True)
     gatherer_start = models.IntegerField(blank=True, null=True)
-    gatherer_stop = models.IntegerField(blank=True, null=True)
+    gatherer_stop  = models.IntegerField(blank=True, null=True)
     
     class Meta:
         verbose_name_plural = "Sets"
